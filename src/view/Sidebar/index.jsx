589a0fd2eed5fb85ab098dbb0253e8c2a75c75b4
tangerinene <tangerinene@gmail.com>
2021-09-26 15:43:06 +0300

    Button table

--- src/view/Sidebar/index.jsx
+++ src/view/Sidebar/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from "./styled";
-
+import { Link } from "react-router-dom";
 export const Sidebar = ({ menuData }) => {
   return (
     <Container isOpen={menuData.isOpen}>
-      <div variant="h3">{menuData?.data?.country || null}</div>
+        <div variant="h3">{menuData?.data?.country || null}</div>
+        <Link to="/table">Таблица</Link>
     </Container>
   );
 };
