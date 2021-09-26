25cc7127a8acd78d294062493e043d1ff6db5de4
tangerinene <tangerinene@gmail.com>
2021-09-26 04:45:34 +0300

    Fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SignIn } from "./view/SignIn";
+import { SignUp } from "./view/SignUp";
 import { Main } from "./view/Main";
+
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route from="/signin" exact>
           <SignIn />
+        </Route>
+        <Route from="/signup" exact>
+          <SignUp />
         </Route>
         <Route from="/" exact>
           <Main />
